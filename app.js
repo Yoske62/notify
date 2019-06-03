aba6932fe60d04c0302de7b220d5957853f040cb
DESKTOP-INGFMFD\Yossi <yossi.levi@kbis.co.il>
2019-06-03 17:15:55 +0300

    s

--- app.js
+++ app.js
@@ -44,11 +44,11 @@ app.get('/messages', function (req, res) {
   const host = req.query.host;
   const ip = getIP(req);
   if (!hosts.find(x => x.host == host)) {
-    hosts.push({host,ip, timestamp: new Date().toLocaleString('he-IL')});
+    hosts.push({host,ip, timestamp: new Date()});
   } else {
     const curr = hosts.find(x => x.ip == ip);
     if (curr) {
-      curr.timestamp = new Date().toLocaleString('he-IL')
+      curr.timestamp = new Date()
     }
   }
   res.statusCode = 200;
